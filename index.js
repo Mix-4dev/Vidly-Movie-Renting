ee6a5811ffb7eeda19f2a539973f64176d37fd1b
Mix <dariusmix880@gmail.com>
2025-06-25 00:36:17 +0300

    GET & POST movies features

--- index.js
+++ index.js
@@ -11,4 +11,23 @@ const movies = [
     price: 15,
   },
 ];
+// Home page
+app.get('/vidly.com', (req, res) => {
+  res.send('Welcome to Vidly for Renting Movies');
+});
 
+app.get('/vidly.com/api/movies', (req, res) => {
+  res.send(movies);
+});
+
+app.post('/vidly.com/api/movies', (req, res) => {
+  const newMovie = {
+    id: movies.length + 1,
+    name: req.body.name
+  };
+  movies.push(newMovie);
+  res.send(movies);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
