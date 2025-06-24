1be8e1c525947410c360df77d35fc23089a14b97
Mix <dariusmix880@gmail.com>
2025-06-25 00:03:31 +0300

    Inital Commit + Express package

---
+++ index.js
@@ -0,0 +1,14 @@
+// Common JS require function
+const express = require('express');
+const app = express();
+
+const movies = [
+  {
+    name: 'The Matrix',
+    genre: 'Action/Sci-fi',
+    date: 1999,
+    rate: 8.7,
+    price: 15,
+  },
+];
+
